53000a6f1c6ee46d5cafffb1006bf53a34afea8d
adithyan-gireeshkumar <gireeshkumaradithyan@gmail.com>
2026-09-17 11:17:52 +0530

    feat: add header and hero components with styles, include SVG assets for React and Vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Header from './components/header/header.jsx'
+import Body from './components/hero-body/body.jsx'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Body/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import './header.css'
+function Header(){
+    return (
+    <header>
+         <div className="container">
+            <a href="#">Peedika</a>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Products</a>
+                <a href="#">About</a>
+            </nav>
+            </div>
+       </header>
+    )
+}
+export default Header;

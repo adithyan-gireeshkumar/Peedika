8110d8b56cfa447e9af7a9d6487308c028160b30
adithyan-gireeshkumar <gireeshkumaradithyan@gmail.com>
2026-09-17 12:22:18 +0530

    feat: add header and hero components with styling
    - Created a new header component with navigation links.
    - Added CSS for header styling.
    - Implemented a hero section with a banner and call-to-action button.
    - Included CSS for hero section layout and design.
    - Set up main entry point for React application.
    - Configured Vite for React with Babel support.
    - Added React logo SVG asset.
    - Established global styles for body and elements.

---
+++ Peedika/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/header/header.jsx'
+import Body from './components/hero-body/body.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+
+    </>
+  )
+}
+
+export default App
---
+++ Peedika/src/components/hero-body/body.jsx
@@ -0,0 +1,21 @@
+import './body.css'
+function Body() {
+  return (
+    <div className="banner-container">
+                <div className="container">
+                    <div className="left-section">
+                        <h1>Peedika</h1>
+            
+                        <h2>Everything you need, in one place</h2>
+            
+                        <h3>Smart choices. Better prices. Delivered to your door</h3>
+            
+                        <button onclick="window.location.href='./pages/login.html'">Shop Now</button>
+
+                    </div>
+
+                </div>
+            </div>
+  )
+}
+export default Body ;
